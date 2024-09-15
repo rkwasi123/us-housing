d2c9d04e563aa1c13cd55de80802b40a0d72d5ce
rkwasi123 <105535551+rkwasi123@users.noreply.github.com>
2024-09-14 20:51:52 -0700

    Add files via upload

---
+++ US Household Income Data Cleaning.sql
@@ -0,0 +1,89 @@
+# US Household Income Data Cleaning
+
+SELECT * 
+FROM us_project.us_household_income;
+
+SELECT * 
+FROM us_project.us_household_income_statistics;
+
+ALTER TABLE us_project.us_household_income_statistics RENAME COLUMN `ï»¿id` TO `id`;
+
+SELECT COUNT(id)
+FROM us_project.us_household_income;
+
+SELECT COUNT(id)
+FROM us_project.us_household_income_statistics;
+
+
+
+SELECT id, COUNT(id)
+FROM us_project.us_household_income
+GROUP BY id
+HAVING COUNT(id) > 1
+;
+
+SELECT*
+FROM(
+SELECT row_id,
+id,
+ROW_NUMBER() OVER(PARTITION BY id ORDER BY id) row_num
+FROM us_project.us_household_income
+) duplicates
+WHERE row_num > 1
+;
+
+DELETE FROM us_household_income
+WHERE row_id IN (
+	SELECT row_id
+	FROM(
+		SELECT row_id,
+		id,
+		ROW_NUMBER() OVER(PARTITION BY id ORDER BY id) row_num
+		FROM us_project.us_household_income
+		) duplicates
+	WHERE row_num > 1)
+;
+
+SELECT DISTINCT State_Name
+FROM us_project.us_household_income
+ORDER BY 1
+;
+
+UPDATE us_project.us_household_income
+SET State_Name = 'Georgia'
+WHERE State_Name = 'georia';
+
+UPDATE us_project.us_household_income
+SET State_Name = 'Alabama'
+WHERE State_Name = 'alabama';
+
+
+SELECT *
+from us_project.us_household_income
+WHERE County = 'Autauga County'
+ORDER BY 1
+;
+
+UPDATE us_household_income
+SET Place = 'Autaugaville'
+WHERE County = 'Autauga County'
+AND City = 'Vinemont'
+;
+
+
+SELECT Type, COUNT(Type)
+FROM us_project.us_household_income
+GROUP BY Type
+;
+
+UPDATE us_household_income
+SET Type = 'Borough'
+WHERE Type = 'Boroughs'
+;
+
+
+SELECT State_Name, ALand, AWater
+FROM us_project.us_household_income
+WHERE (ALand = 0 OR ALand = '' OR ALand IS NULL)
+#AND ALand = 0 OR ALand = '' OR ALand IS NULL
+;
